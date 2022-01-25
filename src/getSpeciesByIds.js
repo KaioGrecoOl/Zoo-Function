61b911e2d8ab0aae8efb6437cbeb870633cbec2b
Kaio G <kaiogreco91@gmail.com>
2022-01-25 17:10:18 -0300

    requisito 1 getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return data.species.filter((specieId) => ids.includes(specieId.id));
 }
 
 module.exports = getSpeciesByIds;
