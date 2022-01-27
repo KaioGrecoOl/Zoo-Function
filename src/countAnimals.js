e376bf9731e9188a55e7f0a542bb4041595e0fba
Kaio G <kaiogreco91@gmail.com>
2022-01-27 17:42:51 -0300

    requisito 5 countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-
+  if (animal === undefined) {
+    let animalList = {};
+    species.forEach((index) => { animalList[index.name] = index.residents.length; });
+    return animalList;
+  }
+  const selectanil = species.find((index) => index.name === animal.specie);
+  const sexAn = selectanil.residents.filter((index) => index.sex === animal.sex);
+  if (animal.sex === undefined) {
+    return selectanil.residents.length;
+  } return sexAn.length;
 }
+console.log(countAnimals({ specie: 'elephants' }));
 
 module.exports = countAnimals;
