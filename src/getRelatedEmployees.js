e01fc59cdd1e2f11451c433b871a7fc6ee530910
Kaio G <kaiogreco91@gmail.com>
2022-01-26 21:23:15 -0300

    requisito 4 getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((maneg) => maneg.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const filterMana = data.employees.filter((idMana) => idMana.managers.includes(managerId));
+    return filterMana.map((fullName) => `${fullName.firstName} ${fullName.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
