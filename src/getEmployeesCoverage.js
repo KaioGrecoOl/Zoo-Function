5c9d0dd8df586ba61fefd42debf22e3fef0889a0
Kaio G <kaiogreco91@gmail.com>
2022-01-31 21:10:56 -0300

    requisito 10 getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,40 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const selectedSpecie = function getSpecies(index) {
+  return species.filter((animal) => index.includes(animal.id));
+};
+
+function getName(index) {
+  return selectedSpecie.map((animal) => animal.name);
 }
+
+function getLocation(index) {
+  return selectedSpecie.map((animal) => animal.location);
+}
+
+function employeeI(index) {
+  const emplo = employees.find((name) => name.firstName === index.name
+  || name.lastName === index.name || name.id === index.id);
+  if (!emplo) {
+    throw new Error('Informações inválidas');
+  }
+  const { firstName, lastName, responsibleFor, id } = emplo;
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: getName(responsibleFor),
+    locations: getLocation(responsibleFor),
+  };
+}
+
+function getEmployeesCoverage(index) {
+  if (!index) {
+    return employees.map((name) => employeeI({ id: name.id }));
+  }
+
+  return employeeI(index);
+}
+console.log(getEmployeesCoverage({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' }));
 
 module.exports = getEmployeesCoverage;
